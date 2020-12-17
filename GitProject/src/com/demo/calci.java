134c93a3db796270a20f09593bd90dacb20193f3
namrata kumar <namrata kumar@DESKTOP-JPHBLVM>
2020-12-17 18:34:23 +0530

    Project inserted

---
+++ GitProject/src/com/demo/calci.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+class calci {
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static int diff(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public static void main(String args[]) {
+		System.out.println(add(10, 20));
+	}
+}
