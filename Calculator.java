3e5db2f61a9e569e616fc253c9add81e13ddcf32
Namrata-Kumar <76149016+Namrata-Kumar@users.noreply.github.com>
2020-12-18 13:48:20 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,8 +5,7 @@ return num1+num2;
  public static int diff(int num1,int num2){
 return num1-num2;
  }
-pub
 public static void main(String args[]){
-system.out .println(add(10,20));
+System.out.println(add(10,20));
   }
 }
