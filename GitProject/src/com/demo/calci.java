35231401a592227e3621ba78eb9e088dbda2d0a7
namrata kumar <namrata kumar@DESKTOP-JPHBLVM>
2020-12-18 16:48:40 +0530

    Added Div

--- GitProject/src/com/demo/calci.java
+++ GitProject/src/com/demo/calci.java
@@ -8,9 +8,13 @@ class calci {
 	public static int diff(int num1, int num2) {
 		return num1 - num2;
 	}
+	public static int div(int num1, int num2) {
+		return num1 / num2;
+	}
 
 	public static void main(String args[]) {
 		System.out.println("Add="+ add(10, 20));
 		System.out.println("Diff="+ diff(10, 20));
+		System.out.println("Div="+ div(10, 20));
 	}
 }
