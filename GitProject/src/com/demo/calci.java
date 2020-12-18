a03968ca703760137ebba6ecf478fd763a35c0a8
namrata kumar <namrata kumar@DESKTOP-JPHBLVM>
2020-12-18 16:20:37 +0530

    Merge br

--- GitProject/src/com/demo/calci.java
+++ GitProject/src/com/demo/calci.java
@@ -8,13 +8,13 @@ class calci {
 	public static int diff(int num1, int num2) {
 		return num1 - num2;
 	}
-	public static int mul(int num1, int num2) {
-		return num1 * num2;
-	}
+//	public static int mul(int num1, int num2) {
+//		return num1 * num2;
+//	}
 
 	public static void main(String args[]) {
 		System.out.println(add(10, 20));
 		System.out.println(diff(10, 20));
-		System.out.println(mul(10, 20));
+//		System.out.println("mul="+ mul(10, 20));
 	}
 }
